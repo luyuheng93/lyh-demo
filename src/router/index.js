0df5a4da50e5ae390830840effea5193bfbfb7b4
lyh <1065088060@qq.com>
2021-08-09 18:32:52 +0800

    完成登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由访问守卫
+router.beforeEach((to, from, next) => {
+  // to 表示将要访问的路径
+  // from 表示从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  // next() 放行   next('/login') 强制跳转
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 如果获取不到token，怎强制跳转到'/login'页面
+  if (!tokenStr) return next('/login')
+  // 获取到token，则放行
+  next()
 })
 
 export default router
